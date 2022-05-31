04b772ef0f5848dc56b1be60bc774f8b1be4a3e5
tiaan-jonker <tiaanj06@gmail.com>
2022-05-31 17:29:03 +1200

    Routes setup with server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const connectDB = require('./config/db')
+
+// Importing routes
+const usersRoute = require('./routes/api/users')
+const authRoute = require('./routes/api/auth')
+const profileRoute = require('./routes/api/profile')
+const postsRoute = require('./routes/api/posts')
 
 const server = express()
 
+// Database connection
+connectDB()
+
 server.get('/', (req, res) => res.send('API running'))
+
+// Define routes
+server.use('/api/users', usersRoute)
+server.use('/api/auth', authRoute)
+server.use('/api/profile', profileRoute)
+server.use('/api/posts', postsRoute)
 
 module.exports = server
