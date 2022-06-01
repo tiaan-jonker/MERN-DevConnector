596bb659895d7701ce6e7ad15edf1d650e76d512
tiaan-jonker <tiaanj06@gmail.com>
2022-06-01 15:24:25 +1200

    Jwt auth added to users returning token

--- config/db.js
+++ config/db.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const config = require('config')
 const db = config.get('mongoURI')
 
-// const db =
-// 'mongodb+srv://tiaanj06:Uomo1947iliad@devconnector.d78jc.mongodb.net/?retryWrites=true&w=majority'
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
--- server/routes/api/v1/users.js
+++ server/routes/api/v1/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 const User = require('../../../models/User')
 
@@ -55,7 +57,25 @@ router.post(
       // save user to database
       await user.save()
 
-      res.send('User registered')
+      // for jwt.sign
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+
+      // check https://jwt.io/ for decoded token
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (error) {
       console.error('Server error: ', error.message)
       res.status(500).send('Server error 500')
